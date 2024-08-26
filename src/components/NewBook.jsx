07184121105c7e9184d88596a19fcf0ee293b83c
Neil <mallarineil9@gmail.com>
2024-08-26 16:01:42 +0800

    Refactor NewBook form

--- src/components/NewBook.jsx
+++ src/components/NewBook.jsx
@@ -26,40 +26,50 @@ const NewBook = () => {
 
   return (
     <div>
+      <h1>add a book</h1>
       <form onSubmit={submit}>
         <div>
-          title
+          <label htmlFor='title'>title </label>
           <input
+            id='title'
             value={title}
             onChange={({ target }) => setTitle(target.value)}
           />
         </div>
         <div>
-          author
+          <label htmlFor='author'>author </label>
           <input
+            id='author'
             value={author}
             onChange={({ target }) => setAuthor(target.value)}
           />
         </div>
         <div>
-          published
+          <label htmlFor='published'>published </label>
           <input
-            type="number"
+            id='published'
+            type='number'
             value={published}
             onChange={({ target }) => setPublished(target.value)}
           />
         </div>
         <div>
+          <label htmlFor='genre'>genre </label>
           <input
+            id='genre'
             value={genre}
             onChange={({ target }) => setGenre(target.value)}
           />
-          <button onClick={addGenre} type="button">
+          {' '}
+          <button
+            onClick={addGenre}
+            type='button'
+          >
             add genre
           </button>
         </div>
-        <div>genres: {genres.join(' ')}</div>
-        <button type="submit">create book</button>
+        <div>added genres: {genres.join(' ')}</div>
+        <button type='submit'>create book</button>
       </form>
     </div>
   )
